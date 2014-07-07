5344f7aac44c483571fd0f8cf089d3b39bb749f8
mjvoracek <mjvoracek@gmail.com>
2014-07-07 16:40:35 -0500

    add album vc

---
+++ CoreDataPractice/MJVAddAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  MJVAddAlbumViewController.h
+//  CoreDataPractice
+//
+//  Created by Matthew Voracek on 7/7/14.
+//  Copyright (c) 2014 VOKAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MJVAddAlbumViewController : UIViewController
+
+@end
---
+++ CoreDataPractice/MJVAddAlbumViewController.m
@@ -0,0 +1,21 @@
+//
+//  MJVAddAlbumViewController.m
+//  CoreDataPractice
+//
+//  Created by Matthew Voracek on 7/7/14.
+//  Copyright (c) 2014 VOKAL. All rights reserved.
+//
+
+#import "MJVAddAlbumViewController.h"
+
+@interface MJVAddAlbumViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *artistField;
+@property (weak, nonatomic) IBOutlet UITextField *titleField;
+
+@end
+
+@implementation MJVAddAlbumViewController
+
+
+
+@end
